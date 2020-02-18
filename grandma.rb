3cf5e6ff7f259453c66fd58e5189a71f4cfd4b94
heyserenadinh Dinh <heyserenadinh@gmail.com>
2020-02-18 00:11:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,8 @@ def speak_to_grandma (phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase
+    return "NO, NOT SINCE 1938!"
   else 
     return "NO, NOT SINCE 1938!"
   end
