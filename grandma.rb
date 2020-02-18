4ea3a73b1899fc6392df223d5e21c5d140c88a97
heyserenadinh Dinh <heyserenadinh@gmail.com>
2020-02-18 00:08:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,8 @@
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.downcase ()
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
   else 
